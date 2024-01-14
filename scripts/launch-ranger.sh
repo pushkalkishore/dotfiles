25bd5bf7b0ff29f502bb829e3ca336ad301899e1
pushkalkishore <kpushkal122@gmail.com>
2024-01-15 00:00:00 +0530

    Added the new fzf script and its function

---
+++ scripts/launch-ranger.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Launch Ranger
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Pushkal
+
+# Documentation:
+# @raycast.author Pushkal
+# @raycast.authorURL https://github.com/pushkalkishore
+
+open -n -a iTerm /opt/homebrew/bin/ranger
